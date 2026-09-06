d17668b88548177cf24eeacdc0675fb75f06d4d2
Mariano J. Portal <67043011+devsiderio@users.noreply.github.com>
2026-09-06 20:59:10 -0300

    feat: add message registration exercise
    Add HTML structure and JavaScript logic to prompt the user for a message, then display the collected data in the console and in an alert.

---
+++ Desafio-2/js/script.js
@@ -0,0 +1,6 @@
+// Pedir al usuario un mensaje para mostrar luego
+let mensaje = prompt("Escribí un mensaje:");
+
+// Mostrar tanto en consola como con un 'alert' el mensaje previamente ingresado
+console.log(mensaje);
+alert(mensaje);
